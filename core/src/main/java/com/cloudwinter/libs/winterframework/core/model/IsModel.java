1b6d14d7d0ed8a613f7364b638d89d4af458f5be
Tomas <tomasdepriede@gmail.com>
2014-11-19 22:50:41 -0300

    Added modules to the project. Added simple base model to work with compilation on jenkins and with auto generation of javadocs.

---
+++ core/src/main/java/com/cloudwinter/libs/winterframework/core/model/IsModel.java
@@ -0,0 +1,33 @@
+package com.cloudwinter.libs.winterframework.core.model;
+
+import java.io.Serializable;
+
+/**
+ * Base interface for the models in your application. <br />
+ * <br />
+ * Please extend all your models from this interface. We strongly recommend to
+ * use a String as the primary Id
+ * 
+ * @author Tomas de Priede
+ * @since 11/19/14 10:40 PM
+ * @version 1.0.0
+ */
+public interface IsModel extends Serializable {
+
+    /**
+     *
+     *
+     * @return the unique id associated with the entity
+     */
+    public String getId();
+
+    /**
+     * Caution! This method should only be used when necessary. Avoid modifying
+     * the id programatically whenever possible since this introduces the
+     * potential for id mismatches, which can lead to other problems.
+     *
+     * @param id
+     *            the unique id associated with the entity.
+     */
+    public void setId(String id);
+}
